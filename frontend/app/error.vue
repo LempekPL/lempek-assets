8974b5b7dc18f50d3032aa2ef88a502e58490296
Lempek <37885098+LempekPL@users.noreply.github.com>
2025-11-15 05:38:18 +0100

    404 error page

---
+++ frontend/app/error.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type {NuxtError} from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+  <Navbar/>
+  <main>
+    <div>
+      <p class="error-code">{{ error.statusCode }}</p>
+      <p>{{ error?.message }}</p>
+      <NuxtLink to="/">
+        <PartButton>
+          Go back home
+        </PartButton>
+      </NuxtLink>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-top: 8rem;
+
+  .error-code {
+    font-size: 4rem;
+  }
+
+  div {
+    background: var(--box-color);
+    width: 20rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    border-radius: 2rem;
+    padding: 2rem;
+  }
+}
+</style>
